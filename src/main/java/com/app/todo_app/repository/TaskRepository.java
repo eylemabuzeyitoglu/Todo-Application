ce5addb52143cdbce019acf6a58e27956f6ede01
Eylem Abuzeyitoglu <166799047+eylemabuzeyitoglu@users.noreply.github.com>
2025-04-13 22:46:00 +0300

    ToDo Application

---
+++ src/main/java/com/app/todo_app/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.app.todo_app.repository;
+
+import com.app.todo_app.models.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task,Long> {
+}
